6c93b610509bc68781dc4c520caef274b91b19f2
Errol Watson <ewatsonrei@gmail.com>
2023-10-01 01:27:27 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `wimberley-wellness`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
